54a8a354b5944b2a91fdcc19fe96b3ba2de31874
viniciusFelipeS <viniciusfelipe.xd@gmail.com>
2021-04-16 22:19:43 -0300

    edit: namefile controller.php

---
+++ app/Controllers/Controller.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controllers;
+
+use DI\Container as DIContainer;
+
+abstract class Controller{
+    protected $container;
+
+    public function __construct(DIContainer $container) {
+        $this->container = $container;
+    }
+
+    public function __get($key)
+    {
+        if ($this->container->{$key}){
+            return $this->container->{$key};
+        }
+    }
+}
