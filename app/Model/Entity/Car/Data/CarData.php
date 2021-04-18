d1f8a5a47c2187c911bd29e87dc10538071aa8bf
viniciusFelipeS <viniciusfelipe.xd@gmail.com>
2021-04-18 06:30:42 -0300

    add: DataBase Class into propriety of  CarData

--- app/Model/Entity/Car/Data/CarData.php
+++ app/Model/Entity/Car/Data/CarData.php
@@ -8,28 +8,34 @@ use PDO;
 
 final class CarData extends Car
 {
+    private DataBase $CarDataBase;
     private const DB_NAME = 'car';
+
+    public function __construct(DataBase $dataBase)
+    {
+        $this->CarDataBase = new $dataBase(self::DB_NAME);
+    }
 
     public function insert(array $data)
     {
         $data['status'] = $this->status;
-        return (new DataBase(self::DB_NAME))->insert($data);
+        return $this->CarDataBase->insert($data);
     }
 
     public function select($id = null)
     {
-        return (new DataBase(self::DB_NAME))->select($id)->fetchAll(PDO::FETCH_ASSOC);
+        return $this->CarDataBase->select($id)->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function delete($id)
     {
         $id  = 'id = ' . $id;
-        return (new DataBase(self::DB_NAME))->delete($id);
+        return $this->CarDataBase->delete($id);
     }
 
     public function update($data, $id)
     {
         $id  = 'id = ' . $id;
-        return (new DataBase(self::DB_NAME))->update($data, $id);
+        return $this->CarDataBase->update($data, $id);
     }
 }
